ca877e1f2eb5a23c020f4c9301708040fd9de325
Daniel <dordan0401@gmail.com>
2021-03-08 12:18:13 -0500

    Added EpisodeService

---
+++ WalkingDead.Models/EpisodeCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkingDead.Models
+{
+    public class EpisodeCreate
+    {
+        [Required]
+        public int EpisodeId { get; set; }
+        [Required]
+        public string EpisodeTitle { get; set; }
+        [Required]
+        public string EpisodeDescription { get; set; }
+        [Required]
+        public DateTime AirDate { get; set; }
+    }
+}
