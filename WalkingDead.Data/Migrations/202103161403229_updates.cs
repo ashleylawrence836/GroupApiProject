5138bdf31ec402d84586d74b319693bda59b999e
khughes0123 <khughes0123@gmailcom>
2021-03-16 13:34:52 -0400

    Location Services and Assets

---
+++ WalkingDead.Data/Migrations/202103161403229_updates.cs
@@ -0,0 +1,28 @@
+﻿namespace WalkingDead.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updates : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Location",
+                c => new
+                    {
+                        LocationId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Description = c.String(nullable: false),
+                        AddedByUserId = c.Guid(nullable: false),
+                    })
+                .PrimaryKey(t => t.LocationId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Location");
+        }
+    }
+}
