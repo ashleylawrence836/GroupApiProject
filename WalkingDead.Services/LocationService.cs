65f7eb4ba6cd768fceb5429a5ba9aa966d1e1075
Daniel <dordan0401@gmail.com>
2021-03-16 14:43:49 -0400

    Added Update and Delete services and LocationTests

--- WalkingDead.Services/LocationService.cs
+++ WalkingDead.Services/LocationService.cs
@@ -35,7 +35,7 @@ namespace WalkingDead.Services
             }
         }
 
-        public List<LocationDetail> GetLocations()
+        public List<LocationListItem> GetLocations()
         {
             using (var context = new ApplicationDbContext())
             {
@@ -44,17 +44,18 @@ namespace WalkingDead.Services
                         .Locations
                         .Select(
                             e =>
-                                new LocationDetail
+                                new LocationListItem
                                 {
-                                    
+                                    LocationID = e.LocationId,
                                     Name = e.Name,
-                                    Description = e.Description
+                                    Description = e.Description,
+                                    
                                 });
                 return query.ToList();
             }
         }
 
-        public LocationDetail GetLocationById(int id)
+        public LocationListItem GetLocationById(int id)
         {
             using (var context = new ApplicationDbContext())
             {
@@ -63,13 +64,47 @@ namespace WalkingDead.Services
                         .Locations
                         .Single(e => e.LocationId == id);
                 return
-                    new LocationDetail
+                    new LocationListItem
                     {
+                        LocationID = entity.LocationId,
                         Name = entity.Name,
                         Description = entity.Description
                     };
             }
         }
 
+        public bool UpdateLocation(LocationEdit model)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                Location updateLocation = context.Locations.Single
+                    (e => e.LocationId == model.LocationID);
+
+                if (model.Name != null)
+                {
+                    updateLocation.Name = model.Name;
+                }
+
+                if (model.Description != null)
+                {
+                    updateLocation.Description = model.Description;
+                }
+
+                return context.SaveChangesAsync().Result >= 1;
+            };
+        }
+
+        public bool DeleteLocation(int locationId)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                Location deleteLocation = context.Locations.Single(
+                    e => e.LocationId == locationId && e.AddedByUserId == _userId);
+
+                context.Locations.Remove(deleteLocation);
+
+                return context.SaveChangesAsync().Result == 1;
+            }
+        }
     }
 }
--- WalkingDead.Tests/LocationTests.cs
+++ WalkingDead.Tests/LocationTests.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using WalkingDead.Data;
 using WalkingDead.Models;
+using WalkingDead.Models.LocationModels;
 using WalkingDead.Services;
+using WalkingDeadCharacters.Data;
 
 namespace WalkingDead.Tests
 {
     [TestClass]
     public class LocationTests
     {
+        //Run these in order please
         [TestMethod]
-        public void TestCreateLocationMethod1()
+        public void TestCreateLocationMethod()
         {
             Guid userId = new Guid();
             LocationCreate testLocation = new LocationCreate()
@@ -25,10 +31,60 @@ namespace WalkingDead.Tests
         }
 
         [TestMethod]
+        public void TestGetLocationsMethod()
+        {
+            Guid userId = new Guid();
+            LocationService service = new LocationService(userId);
+
+            List<LocationListItem> locations = service.GetLocations();
+
+            Assert.IsTrue(locations.Count() > 0);
+        }
+
+        [TestMethod]
+        public void TestGetLocationByIdMethod()
+        {
+            Guid userId = new Guid();
+            LocationService service = new LocationService(userId);
+            LocationListItem testLocation =
+            service.GetLocationById(1);
+
+            Assert.AreEqual(1, testLocation.LocationID);
+        }
+
+        [TestMethod]
         public void TestUpdateLocationMethod()
         {
             Guid userId = new Guid();
-            
+            LocationEdit model = new LocationEdit()
+            {
+                LocationID = 1,
+                Name = "A CHURCH"
+            };
+
+            LocationService service = new LocationService(userId);
+            bool wasUpdated = service.UpdateLocation(model);
+
+            Assert.IsTrue(wasUpdated);
+        }
+
+        [TestMethod]
+        public void TestDeleteLocationMethod()
+        {
+            Guid userId = new Guid();
+            LocationService service = new LocationService(userId);
+
+            using (var context = new ApplicationDbContext())
+            {
+                Location testLocation = context.Locations.Single(
+                    e => e.LocationId == 1);
+                userId = testLocation.AddedByUserId;
+
+                bool wasDeleted = service.DeleteLocation(1);
+
+                Assert.IsTrue(wasDeleted);
+            }
+
         }
     }
 }
